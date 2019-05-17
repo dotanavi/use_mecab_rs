936f78105eff82604c85bfb82e9b79128b6261e0
Ota Daichi <daichi.ota@naviplus.co.jp>
2019-05-17 17:01:17 +0900

    not work

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
+use std::ptr;
+use std::ffi::{CStr, CString};
 
-
-#[repr(C)]
-struct Mecab(*const libc::c_void);
+enum Mecab {}
+type MecabPtr = *const Mecab;
+type CharPtr = *const libc::c_char;
 
 #[link(name = "mecab")]
 extern "C" {
-    fn mecab_new(argc: libc::c_int, argv: *const *const libc::c_char) -> Mecab;
-    fn mecab_destroy(mecab: Mecab);
+    // fn mecab_new(argc: libc::c_int, argv: *const CharPtr) -> MecabPtr;
+    fn mecab_new2(arg: CharPtr) -> MecabPtr;
+    fn mecab_destroy(mecab: MecabPtr);
+
+    fn mecab_strerror(mecab: MecabPtr) -> CharPtr;
+
+    fn mecab_sparse_tostr(mecab: MecabPtr, str: CharPtr) -> CharPtr;
+}
+
+fn print(s: CharPtr) {
+    let s = unsafe { CStr::from_ptr(s) };
+    match s.to_str() {
+        Ok(s) => println!("{}", s),
+        Err(e) => println!("{}", e),
+    }
+}
+
+unsafe fn main2(mecab: MecabPtr) {
+    let input = CString::new("太郎は次郎が持っている本を花子に渡した。").unwrap();
+    let result = mecab_sparse_tostr(mecab, input.as_ptr());
+    if result == ptr::null() {
+        let err_msg = mecab_strerror(mecab);
+        print(err_msg);
+        return;
+    }
+    print(result);
 }
 
 fn main() {
     unsafe {
-        let mecab = mecab_new(0, std::ptr::null());
-        println!("Hello, world!");
+        let input = CString::new("-d /usr/local/lib/mecab/dic/ipadic").unwrap();
+        let mecab = mecab_new2(input.as_ptr());
+        if mecab == ptr::null() {
+            println!("mecabのインスタンスの作成に失敗しました");
+            print(mecab_strerror(ptr::null()));
+            return;
+        }
+        main2(mecab);
         mecab_destroy(mecab);
     }
 }
